189058c809508e04c26cd85c288625ba8965b1bc
winghazelcheng <winghazel.cheng26@ycdsbk12.ca>
2025-01-06 09:21:56 -0500

    Console
    Named console and fulfilled dimension requirements

--- HazelCPTGuessTheWord.java
+++ HazelCPTGuessTheWord.java
@@ -2,6 +2,6 @@ import arc.*;
 
 public class HazelCPTGuessTheWord{
 	public static void main(String[] args){
-		Console con = new Console();
+		Console con = new Console("Guess the Word", 1280, 720);
 	}
 }
