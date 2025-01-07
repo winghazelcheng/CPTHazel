be22bccfadb296a12e32affef6b6df4fac0f35ad
winghazelcheng <winghazel.cheng26@ycdsbk12.ca>
2025-01-07 09:23:24 -0500

    Main Menu
    Created Main Menu with getChar

--- HazelCPTGuessTheWord.java
+++ HazelCPTGuessTheWord.java
@@ -3,5 +3,36 @@ import arc.*;
 public class HazelCPTGuessTheWord{
 	public static void main(String[] args){
 		Console con = new Console("Guess the Word", 1280, 720);
+		
+		char chrMainMenu;
+		String strTheme;
+		String strHighScores;
+		
+		con.println("Play game(p)");
+		con.println("View high scores(v)");
+		con.println("Quit(q)");
+		con.println("Help(h)");
+		chrMainMenu = con.getChar();
+		
+		con.clear();
+		
+		if(chrMainMenu == 'p'){
+			TextInputFile themes = new TextInputFile("theme.txt");
+			while(themes.eof() == false){
+				strTheme = themes.readLine();
+				con.println(strTheme);
+			}
+		}else if(chrMainMenu == 'v'){
+			TextInputFile highscores = new TextInputFile("highscores.txt");
+			while(highscores.eof() == false){
+				strHighScores = highscores.readLine();
+				con.println(strHighScores);
+			}
+		}else if(chrMainMenu == 'q'){
+			con.closeConsole();
+		}else if(chrMainMenu == 'h'){
+			con.println("help");
+		}else{
+		}
 	}
 }
