eecbfe7f0ebd082787e16eead6d808c464d7d83a
winghazelcheng <winghazel.cheng26@ycdsbk12.ca>
2025-01-08 09:55:06 -0500

    Themes
    Read from console chosen theme
    Put word s in theme file to array
    generate random number put into array

--- HazelCPTGuessTheWord.java
+++ HazelCPTGuessTheWord.java
@@ -8,6 +8,7 @@ public class HazelCPTGuessTheWord{
 		String strTheme;
 		String strHighScores;
 		boolean binInvalid;
+		String strChooseTheme;
 		
 		binInvalid = true;
 		chrMainMenu = ' ';
@@ -22,6 +23,40 @@ public class HazelCPTGuessTheWord{
 					strTheme = themes.readLine();
 					con.println(strTheme);
 				}
+				themes.close();
+				con.println("Enter theme:");
+				strChooseTheme = con.readLine();
+				strTheme  = "";
+				themes = new TextInputFile("theme.txt");
+				while(themes.eof() == false && strChooseTheme != strTheme){
+					strTheme = themes.readLine();
+					if(strChooseTheme.equalsIgnoreCase(strTheme)){
+						TextInputFile chosentheme = new TextInputFile(strTheme+".txt");
+						themes.close();
+						con.clear();
+						int intAmount = 0;
+						while(chosentheme.eof() == false){
+							String strWord;
+							strWord = chosentheme.readLine();
+							intAmount = intAmount + 1;
+							con.println(strWord);
+							con.println(intAmount);
+						}
+						chosentheme.close();
+						String strWords[][];
+						strWords = new String[intAmount][2];
+						chosentheme = new TextInputFile(strTheme+".txt");
+						int intRow;
+						for(intRow = 0; intRow < intAmount; intRow++){
+							String strNumber;
+							strWords[intRow][0] = chosentheme.readLine();
+							int intRandom = (int)(Math.random());
+							strWords[intRow][1] = Integer.toString(intRandom);
+						}
+						
+					}
+				}					
+				
 			}else if(chrMainMenu == 'v'){
 				binInvalid = false;
 				con.println("High Scores");
